e5c3d223662fd791c04a0e1ee965f05579dcebb4
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-04-29 21:01:45 +0300

    feat: Create 2 new components

---
+++ src/app/components/eperson-template-driven-form/eperson-template-driven-form.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import{FormsModule, NgForm} from '@angular/forms'
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+@Component({
+  selector: 'app-eperson-template-driven-form',
+  imports: [
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule
+  ],
+  templateUrl: './eperson-template-driven-form.component.html',
+  styleUrl: './eperson-template-driven-form.component.css'
+})
+export class EpersonTemplateDrivenFormComponent {
+
+}
