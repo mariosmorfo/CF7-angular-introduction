697a27ab64bf83d945cc903f2f6c455428a93272
Marios.Morfonidis <marios.morfonidis98@gmail.com>
2025-04-30 20:10:39 +0300

    Creation of template driven form pass data to child components

--- src/app/components/eperson-template-driven-form/eperson-template-driven-form.component.ts
+++ src/app/components/eperson-template-driven-form/eperson-template-driven-form.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
 import{FormsModule, NgForm} from '@angular/forms'
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { EPerson } from 'src/app/shared/interfaces/eperson';
 @Component({
   selector: 'app-eperson-template-driven-form',
   imports: [
+    CommonModule,
     FormsModule,
     MatButtonModule,
     MatFormFieldModule,
@@ -17,5 +20,25 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './eperson-template-driven-form.component.css'
 })
 export class EpersonTemplateDrivenFormComponent {
+  @Output() person = new EventEmitter<EPerson>()
+  @ViewChild('eform', {static:false}) form:NgForm | undefined;
 
+  onSubmit(value: EPerson){
+    console.log(value);
+    console.log(this.form);
+    console.log(this.form?.form.get('givenName')?.value)
+    console.log(this.form?.form.controls['surName'].value);
+    this.person.emit(value);
+  }
+
+  onSetValue(){
+    this.form?.setValue({
+      givenName:"John",
+      surName:"Doe",
+      age: 30,
+      email:"john@aueb.gr",
+      education:"Bachelor's degree"
+    });
+    this.form?.form.controls['givenName'].setValue("aaaaa")
+  }
 }
